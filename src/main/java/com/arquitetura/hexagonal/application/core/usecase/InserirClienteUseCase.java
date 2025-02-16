beecb9b578b21ab7186fb83291b3989c1c2ec6ae
Fábio Ferreira <fabio_junior1994@hotmail.com>
2025-02-15 22:18:37 -0300

    Criação de use cases para inserir cliente e buscar endereço pelo cep.

---
+++ src/main/java/com/arquitetura/hexagonal/application/core/usecase/InserirClienteUseCase.java
@@ -0,0 +1,23 @@
+package com.arquitetura.hexagonal.application.core.usecase;
+
+import com.arquitetura.hexagonal.application.core.domain.Cliente;
+import com.arquitetura.hexagonal.application.ports.out.BuscarEnderecoPeloCepOutputPort;
+import com.arquitetura.hexagonal.application.ports.out.InserirClienteOutputPort;
+
+public class InserirClienteUseCase {
+
+    private final BuscarEnderecoPeloCepOutputPort buscarEnderecoPeloCepOutputPort;
+    private final InserirClienteOutputPort insertCustomerOutputPort;
+
+    public InserirClienteUseCase(BuscarEnderecoPeloCepOutputPort buscarEnderecoPeloCepOutputPort,
+                                 InserirClienteOutputPort insertCustomerOutputPort) {
+        this.buscarEnderecoPeloCepOutputPort = buscarEnderecoPeloCepOutputPort;
+        this.insertCustomerOutputPort = insertCustomerOutputPort;
+    }
+
+    public void inserir(Cliente cliente, String cep) {
+        var endereco = buscarEnderecoPeloCepOutputPort.buscar(cep);
+        cliente.setEndereco(endereco);
+        insertCustomerOutputPort.inserir(cliente);
+    }
+}
